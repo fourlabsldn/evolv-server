70ddb8898e814c02af11d725ad0e450da13ed875
lazamar <lazaronijunior@gmail.com>
2016-05-06 17:48:33 +0100

    Wip

--- models/Property.js
+++ models/Property.js
@@ -7,34 +7,37 @@ const Types = keystone.Field.Types;
  */
 
 const Property = new keystone.List('Property', {
-  autokey: { path: 'key', from: 'name', unique: true }
+  autokey: { from: 'postcode', path: 'key', unique: true }
 });
 
 Property.add({
-  name: { type: String, required: true },
+  price: { type: Number },
   sell: { type: Boolean },
   rent: { type: Boolean },
   bedrooms: { type: Number },
   size: { type: Number },
-  type: { type: Types.Select, options: [
-    { value: 'apartment', label: 'House' },
-		{ value: 'house', label: 'Apartment' },
-  ] },
-  summary: { type: Boolean, wysiwyg: true, height: 150 },
-  description: { type: String, wysiwyg: true, height: 400 },
-  location: { type: String },
-  ownership: { type: Types.Select, options: [
-    { value: 'freehold', label: 'Freehold' },
-		{ value: 'leasehold', label: 'Leasehold' },
-  ] },
-  address: {
-    postcode: { type: String },
-    houseNumber: { type: String },
-    street: { type: String },
-    area: { type: String },
-    city: { type: String },
-    country: { type: String },
-  }
+  type: {
+    type: Types.Select,
+    default: 0,
+    index: true,
+    options: [
+      { value: 'apartment', label: 'House' },
+      { value: 'house', label: 'Apartment' },
+      { value: 'office', label: 'Office' },
+    ] },
+  summary: { type: Types.Html, wysiwyg: true, height: 150 },
+  description: { type: Types.Html, wysiwyg: true, height: 400 },
+  ownership: {
+    type: Types.Select, options: [
+      { value: 'freehold', label: 'Freehold' },
+      { value: 'leasehold', label: 'Leasehold' },
+    ] },
+  postcode: { type: String, required: true, initial: true },
+  houseNumber: { type: String },
+  street: { type: String },
+  area: { type: String },
+  city: { type: String },
+  country: { type: String },
 });
 
 Property.defaultColumns = 'name';
--- routes/views/search.js
+++ routes/views/search.js
@@ -1,14 +1,35 @@
-var keystone = require('keystone');
+const keystone = require('keystone');
 
-exports = module.exports = function(req, res) {
+exports = module.exports = (req, res) => {
+  const view = new keystone.View(req, res);
+  const locals = res.locals;
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  // TODO: Make this 'rent' or 'buy' instead of search
+  locals.section = 'search';
+  locals.filters = {
+    category: req.params.category
+  };
+  locals.data = {};
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'search';
+  // Load all properties
+  view.on('init', (next) => {
+    keystone.list('Property')
+      .model.find()
+      .exec((err, results = []) => {
+        if (err || !results.length) {
+          return next(err);
+        }
 
-	// Render the view
-	view.render('search', { layout: 'public' });
+        locals.data.properties = results;
+
+        return next(err);
+      });
+  });
+
+  // Render the view
+  view.render('search', {
+    layout: 'public'
+  });
 };
