b96b628fc8c61cb5c827774ed8f0b718f8290902
lazamar <lazaronijunior@gmail.com>
2016-05-09 14:05:28 +0100

    Allow multiple property images

--- models/Property.js
+++ models/Property.js
@@ -12,7 +12,7 @@ const Property = new keystone.List('Property', {
 });
 
 Property.add({
-	image: { type: Types.CloudinaryImage },
+	image: { type: Types.CloudinaryImages },
 	floorPlan: { type: Types.CloudinaryImage },
 	epc: { type: Types.CloudinaryImage },
   price: { type: Number },
