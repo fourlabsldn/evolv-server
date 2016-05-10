2401eeda81a23c98b3920792d8dfbaa11d3a4075
lazamar <lazaronijunior@gmail.com>
2016-05-10 17:24:21 +0100

    Finish main form styles

--- routes/views/register.js
+++ routes/views/register.js
@@ -7,16 +7,55 @@ exports = module.exports = (req, res) => {
   locals.data.formTitle = 'Register';
   locals.data.formFields = [
     {
-      type: 'text'
+      type: 'select',
+      name: 'sellRent',
+      required: true,
+      options: [
+        { text: 'Sell or Rent', disabled: true, selected: true },
+        { text: 'Sell' },
+        { text: 'Rent' }
+      ]
     },
     {
-      type: 'select'
+      type: 'text',
+      required: true,
+      placeholder: 'First Name',
+      name: 'firstName'
     },
     {
-      type: 'select'
+      type: 'text',
+      placeholder: 'Last Name',
+      name: 'lastName'
+    },
+    {
+      type: 'email',
+      placeholder: 'Email',
+      name: 'email'
+    },
+    {
+      type: 'text',
+      placeholder: 'Telephone',
+      name: 'telephone'
+    },
+    {
+      type: 'text',
+      placeholder: 'Maximum price',
+      name: 'maxPrice'
+    },
+    {
+      type: 'text',
+      placeholder: 'Minimum price',
+      name: 'minPrice'
+    },
+
+    {
+      type: 'textarea',
+      placeholder: 'Any other requirements',
+      name: 'otherRequirements',
     }
   ];
 
+  addTagPropertyToFields(locals.data.formFields);
   const viewName = 'register';
 
   locals.section = viewName;
@@ -25,3 +64,16 @@ exports = module.exports = (req, res) => {
     layout: 'public'
   });
 };
+
+
+function addTagPropertyToFields(formFields) {
+  formFields.forEach((field) => {
+    if (field.tag) { return; }
+    if (field.type === 'select' ||
+        field.type === 'textarea') {
+      field.tag = field.type;
+    } else {
+      field.tag = 'input';
+    }
+  });
+}
