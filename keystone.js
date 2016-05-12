eb7e2d3cc52555ff66fb56237fe4bba3223643d2
lazamar <lazaronijunior@gmail.com>
2016-05-12 10:11:30 +0100

    Change admin ui footer and data grouping

--- keystone.js
+++ keystone.js
@@ -95,9 +95,10 @@ keystone.set('email tests', require('./routes/emails'));
 // Configure the navigation bar in Keystone's Admin UI
 
 keystone.set('nav', {
-	'posts': ['posts', 'post-categories'],
-	'enquiries': 'enquiries',
-	'users': 'users'
+	'forms': ['enquiries', 'registrations', 'valuation-requests'],
+  'properties': 'properties',
+  'blog': ['posts', 'post-categories'],
+  'users': 'users'
 });
 
 // Start Keystone to connect to your database and initialise the web server
