36f60a1dffe9106c5952559ba9934aeedbae1ee7
lazamar <lazaronijunior@gmail.com>
2016-05-16 10:56:08 +0100

    Add sell model

---
+++ models/pages/Sell.js
@@ -0,0 +1,56 @@
+/* eslint-disable max-len*/
+
+const keystone = require('keystone');
+const Types = keystone.Field.Types;
+
+/**
+ * Sell Model
+ * =============
+ */
+
+const Sell = new keystone.List('Sell', {
+	// nocreate: true,
+  // nodelete: true
+});
+
+Sell.add(
+  'Section 1',
+  {
+    section_1: {
+      main_heading: { type: Types.Text, required: true, default: 'SELLING YOUR PROPERTY', label: 'Main heading' },
+      main_subheading: { type: Types.Text, required: true, default: 'Exceptional service, outstanding results', label: 'Main subheading' }
+    }
+  },
+  'Sectio 2',
+  {
+    section_2: {
+      heading: { type: Types.Text, required: true, default: 'A very personal approach', label: 'Heading' },
+      content: { type: Types.Html, wysiwyg: true, height: 400, label: 'Content' }
+    }
+  },
+  'Section 3',
+  {
+    section_3: {
+      heading: { type: Types.Text, required: true, default: 'What we do', label: 'Heading' },
+      content: { type: Types.Html, wysiwyg: true, height: 400, label: 'Content' },
+      column_1: {
+        heading: { type: Types.Text, required: true, default: 'Selling only', label: 'Column 1 - Heading' },
+        content: { type: Types.Html, wysiwyg: true, height: 400, label: 'Column 1 - Content' }
+      },
+      column_2: {
+        heading: { type: Types.Text, required: true, default: 'Full Management', label: 'Column 2 - Heading' },
+        content: { type: Types.Html, wysiwyg: true, height: 400, label: 'Column 2 - Content' }
+      }
+    }
+  },
+  'Section 4',
+  {
+    section_4: {
+      heading: { type: Types.Text, required: true, default: 'Looking to sell?', label: 'Heading' },
+      content: { type: Types.Html, wysiwyg: true, default: 'Register your details and tell us what you are looking for', height: 400, label: 'Content' }
+    }
+  }
+);
+
+Sell.defaultColumns = 'section_1.main_heading, section_1.main_subheading';
+Sell.register();
