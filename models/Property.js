246b403e9e81c2d0a57f94a0fe5c7d13232ae23c
lazamar <lazaronijunior@gmail.com>
2016-05-17 15:30:48 +0100

    Make Sell and rent price dependent on availability

--- models/Property.js
+++ models/Property.js
@@ -1,23 +1,31 @@
 const keystone = require('keystone');
 const Types = keystone.Field.Types;
 
+// CONSTANTS
+const placeholderImage = '/img/property-placeholder.svg';
 /**
  * Property Model
  * ==========
  */
 
 const Property = new keystone.List('Property', {
-	map: { name: 'postcode' },
+	map: { name: 'location.street1' },
 	autokey: { path: 'slug', from: 'postcode', unique: true }
 });
 
 Property.add({
+  location: { type: Types.Location, required: true, initial: true },
 	images: { type: Types.CloudinaryImages },
 	floorPlan: { type: Types.CloudinaryImage },
 	epc: { type: Types.CloudinaryImage },
-  price: { type: Number },
-  sell: { type: Boolean },
-  rent: { type: Boolean },
+  sell: {
+    available: { type: Boolean },
+    price: { type: Types.Money, currency: 'en-gb', dependsOn: { 'sell.available': true } }
+  },
+  rent: {
+    available: { type: Boolean },
+    price: { type: Types.Money, currency: 'en-gb', dependsOn: { 'rent.available': true } }
+  },
   bedrooms: { type: Number },
   size: { type: Number },
   type: {
@@ -28,41 +36,28 @@ Property.add({
       { value: 'house', label: 'Apartment' },
       { value: 'office', label: 'Office' }
     ] },
+    ownership: {
+      type: Types.Select, options: [
+        { value: 'freehold', label: 'Freehold' },
+        { value: 'leasehold', label: 'Leasehold' }
+      ] },
   summary: { type: Types.Html, wysiwyg: true, height: 150 },
 	description: { type: Types.Html, wysiwyg: true, height: 400 },
-  locationDescription: { type: Types.Html, wysiwyg: true, height: 400 },
-  ownership: {
-    type: Types.Select, options: [
-      { value: 'freehold', label: 'Freehold' },
-      { value: 'leasehold', label: 'Leasehold' }
-    ] },
-  location: { type: Types.Location },
-  postcode: { type: String, required: true, initial: true },
-  houseNumber: { type: String },
-  street: { type: String },
-  city: { type: String },
-  country: { type: String }
-});
-
-
-// Adds placeholder image url and creates an array of image URLs.
-const placeholderImage = '/img/property-placeholder.svg';
-
-Property.schema.virtual('images_urls').get(function () {
-  if (!this.images || this.images.length === 0) {
-    return [placeholderImage];
-  }
-
-	return this.images.map(image => image.secure_url);
+  locationDescription: { type: Types.Html, wysiwyg: true, height: 400 }
 });
 
 Property.schema.pre('save', function (next) {
-  // Get geolocation data
+  this.placeholderImage = placeholderImage;
+  next();
+});
+
+Property.schema.pre('save', function (next) {
+  // Insert geolocation data
   const region = 'UK';
   const updateRecord = true;
   this._.location.googleLookup(region, updateRecord, () => {});
   next();
 });
 
-Property.defaultColumns = 'street, houseNumber|20%, postcode|20%, type|20%';
+Property.defaultColumns = 'location|70%, type|20%';
 Property.register();
