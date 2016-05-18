e6ef25449d7ebc70ccae9b42ce9cbcd0be0bb03b
lazamar <lazaronijunior@gmail.com>
2016-05-18 10:53:56 +0100

    Load featured properties from the database

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const keystone = require('keystone');
 
 /**
  * Executes a query in a database model
- * @method exports
+ * @method findWhere
  * @param  {String} criterion - A property name of the model
  * @param  {} value - Antything the model property value should be.
  * @return {Promise} - Will resolve into an array of items.
@@ -17,6 +17,27 @@ keystone.List.prototype.findWhere = function findWhere(criterion, value) {
   return new Promise((resolve, reject) => {
     const model = this.model;
     const query = model.find().where(criterion, value);
+
+    query.exec((err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+
+/**
+ * Returns a promise to be resolved with all database records
+ * @method getAll
+ * @return {Promise} - Will resolve into an array of items.
+ */
+keystone.List.prototype.getAll = function getAll() {
+  return new Promise((resolve, reject) => {
+    const model = this.model;
+    const query = model.find();
 
     query.exec((err, results) => {
       if (err) {
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,7 +1,6 @@
 const keystone = require('keystone');
 
-exports = module.exports = function(req, res) {
-
+exports = module.exports = function (req, res) {
 	const view = new keystone.View(req, res);
 	const locals = res.locals;
 
@@ -11,19 +10,32 @@ exports = module.exports = function(req, res) {
 	locals.data = {};
 
 	// Load page content from database
-  view.on('init', (next) => {
-    keystone.list('Home')
-      .model.find()
-      .exec((err, result) => {
-        if (Array.isArray(result)) {
-          locals.data = result[0];
-        }
-        return next(err);
-      });
-  });
+  view.on('init', next => loadHomeContent(next, locals));
 
   const viewName = 'index';
 	// Render the view
   locals.section = viewName;
 	view.render(viewName, { layout: 'public' });
 };
+
+function loadHomeContent(next, locals) {
+  const queries = [
+    keystone.list('Home').getAll(),
+    keystone.list('Property').getFeatured()
+  ];
+
+  // Add loaded content to 'locals' to make it accessible to the view
+  Promise.all(queries)
+  .then((homeContent, featuredProperties) => {
+    if (Array.isArray(homeContent)) {
+      locals.data = homeContent[0];
+    }
+
+    if (Array.isArray(featuredProperties)) {
+      locals.featuredProperties = featuredProperties;
+    }
+
+    return next();
+  })
+  .catch(next);
+}
