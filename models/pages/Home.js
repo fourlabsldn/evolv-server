52c277b7245e26cd7db4b8c754cb8e50579a75ef
Jonny Schmid <hi@jonathanschmid.de>
2016-05-20 15:21:50 +0100

    Remove a tab from homepage

--- models/pages/Home.js
+++ models/pages/Home.js
@@ -25,22 +25,21 @@ Home.add(
   {
     section_2: {
       heading: { type: Types.Text, required: true, default: 'A little history of Evolv.', label: 'Heading' },
-      content: { type: Types.Textarea, height: 500, label: 'Content'  },
+      content: { type: Types.Textarea, height: 500, label: 'Content' },
       tab_1: {
         title: { type: Types.Text, required: true, default: 'Item 1', label: 'Tab 1 - Heading' },
+				icon: { type: Types.Text, required: true, default: 'street-view', label: 'Tab 1 - Icon' },
         content: { type: Types.Textarea, height: 400, label: 'Tab 1 - Content' }
       },
       tab_2: {
         title: { type: Types.Text, required: true, default: 'Item 2', label: 'Tab 2 - Heading' },
+				icon: { type: Types.Text, required: true, default: 'university', label: 'Tab 2 - Icon' },
         content: { type: Types.Textarea, height: 400, label: 'Tab 2 - Content' }
       },
       tab_3: {
         title: { type: Types.Text, required: true, default: 'Item 3', label: 'Tab 3 - Heading' },
+				icon: { type: Types.Text, required: true, default: 'asterisk', label: 'Tab 3 - Icon' },
         content: { type: Types.Textarea, height: 400, label: 'Tab 3 - Content' }
-      },
-      tab_4: {
-        title: { type: Types.Text, required: true, default: 'Item 4', label: 'Tab 4 - Heading' },
-        content: { type: Types.Textarea, height: 400, label: 'Tab 4 - Content' }
       }
     }
   },
