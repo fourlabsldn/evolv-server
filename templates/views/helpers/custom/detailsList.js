59911e7a712066469bc6dc7ac23d25c529d7d0b1
lazamar <lazaronijunior@gmail.com>
2016-05-23 17:09:02 +0100

    Whip

--- templates/views/helpers/custom/detailsList.js
+++ templates/views/helpers/custom/detailsList.js
@@ -1,6 +1,5 @@
 module.exports = function detailsList(details = []) {
   let list = '';
-  console.dir(Object.keys(details));
   for (const key of Object.keys(details)) {
     const text = details[key];
     if (!text || typeof text !== 'string') {
