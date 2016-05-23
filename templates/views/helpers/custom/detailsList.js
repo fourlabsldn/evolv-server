6370a81f320a364519e0be4f3c945381ca7969a8
lazamar <lazaronijunior@gmail.com>
2016-05-23 17:08:29 +0100

    Fix extra bullet point problem

--- templates/views/helpers/custom/detailsList.js
+++ templates/views/helpers/custom/detailsList.js
@@ -1,8 +1,9 @@
 module.exports = function detailsList(details = []) {
   let list = '';
+  console.dir(Object.keys(details));
   for (const key of Object.keys(details)) {
     const text = details[key];
-    if (typeof text !== 'string') {
+    if (!text || typeof text !== 'string') {
       continue;
     }
     list += `\n<li>${text}</li>`;
